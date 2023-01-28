5737ec9b32942a5b0b4b233e689767df2d492752
xXpuma99Xx <51341582+xXpuma99Xx@users.noreply.github.com>
2023-01-28 15:34:09 -0600

    input output dto config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors();
+  await app.listen(process.env.API_PORT || 3000);
 }
 bootstrap();
