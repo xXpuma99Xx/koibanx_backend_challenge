68bac845909faae8cdea9f1c8b047a0d20194162
xXpuma99Xx <51341582+xXpuma99Xx@users.noreply.github.com>
2023-01-28 15:40:33 -0600

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('encuestas_api')
+    .setDescription('Api de encuestas')
+    .setVersion('1.0')
+    .addBearerAuth({ bearerFormat: 'JWT', type: 'http' }, 'jwt')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
 
+  SwaggerModule.setup('/api', app, document);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
+  console.log(process.env.API_PORT);
   await app.listen(process.env.API_PORT || 3000);
 }
 bootstrap();
